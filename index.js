44fdde54234dd4333701be917c24e6f798c8f2ec
Chunkygoo <calvintai0402@gmail.com>
2023-01-07 18:58:36 -0800

    test

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 // // const { Noco } = require('nocodb-daily');
 // // const { Noco } = require('nocodb');
 
-// // (async () => {
-// //   try {
-// //     const { Noco } = require('nocodb-daily');
-// //     const httpServer = app.listen(process.env.PORT || 8080);
-// //     app.use(await Noco.init({}, httpServer, app));
-// //   } catch (e) {
-// //     console.log(e);
-// //   }
-// // })();
+// (async () => {
+//   try {
+//     const { Noco } = require('nocodb-daily');
+//     const httpServer = app.listen(process.env.PORT || 8080);
+//     app.use(await Noco.init({}, httpServer, app));
+//   } catch (e) {
+//     console.log(e);
+//   }
+// })();
 
 // const httpServer = app.listen(process.env.PORT || 8080);
 
@@ -31,20 +31,30 @@
 
 let app = require('express')();
 
-const PORT = 8080;
+// const PORT = 8080;
 
-const httpServer = app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+// const httpServer = app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `);
+// });
 
-app.use(async function (req, res, next) {
+// app.use(async function (req, res, next) {
+//   try {
+//     const { Noco } = require('nocodb-daily');
+//     await Noco.init({}, httpServer, app);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+(async () => {
   try {
     const { Noco } = require('nocodb-daily');
-    await Noco.init({}, httpServer, app);
-    next();
-  } catch (error) {
-    next(error);
+    const httpServer = app.listen(process.env.PORT || 8080);
+    app.use(await Noco.init({}, httpServer, app));
+  } catch (e) {
+    console.log(e);
   }
-});
+})();
 
 module.exports = app;
