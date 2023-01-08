e0850e4ed760cfba1f0c4bec8efc0030d6a4d055
Chunkygoo <calvintai0402@gmail.com>
2023-01-07 22:37:57 -0800

    push to railway

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// let app = require('express')();
-// const { Noco } = require('nocodb-daily');
-// // const { Noco } = require('nocodb');
+const app = require('express')();
+const { Noco } = require('nocodb-daily');
 
 (async () => {
   try {
-    const { Noco } = require('nocodb-daily');
     const httpServer = app.listen(process.env.PORT || 8080);
     app.use(await Noco.init({}, httpServer, app));
   } catch (e) {
@@ -12,68 +10,4 @@
   }
 })();
 
-// const httpServer = app.listen(process.env.PORT || 8080);
-
-// // app.use(await Noco.init({}, httpServer, app));
-
-// app.use(async function (req, res, next) {
-//   try {
-//     // const { Noco } = require('nocodb-daily');
-//     const { Noco } = require('nocodb');
-//     await Noco.init({}, httpServer, app);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// module.exports = app;
-
-// let app = require('express')();
-
-// const PORT = 8080;
-
-// const httpServer = app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
-
-// app.use(async function (req, res, next) {
-//   try {
-//     const { Noco } = require('nocodb-daily');
-//     await Noco.init({}, httpServer, app);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// (async () => {
-//   try {
-//     const { Noco } = require('nocodb-daily');
-//     const httpServer = app.listen(process.env.PORT || 8080);
-//     app.use(await Noco.init({}, httpServer, app));
-//   } catch (e) {
-//     console.log(e);
-//   }
-// })();
-
-// (async () => {
-//   try {
-//     const httpServer = app.listen(process.env.PORT || 8080);
-//     app.use(await Noco.init({}, httpServer, app));
-//   } catch (e) {
-//     console.log(e);
-//   }
-// })();
-
-// app.get('*', async (req, res) => {
-//   // try {
-//   //   const { Noco } = require('nocodb-daily');
-//   //   app.use(await Noco.init({}, httpServer, app));
-//   // } catch (e) {
-//   //   console.log(e);
-//   // }
-//   res.send('All other routes');
-// });
-
-// module.exports = app;
+module.exports = app;
