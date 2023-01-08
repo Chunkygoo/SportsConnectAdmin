15d13c3f3b72053c8a0d5624fbc0bd94b5a1afcf
Chunkygoo <calvintai0402@gmail.com>
2023-01-07 18:39:48 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// let app = require('express')();
+// // const { Noco } = require('nocodb-daily');
+// // const { Noco } = require('nocodb');
+
+// // (async () => {
+// //   try {
+// //     const { Noco } = require('nocodb-daily');
+// //     const httpServer = app.listen(process.env.PORT || 8080);
+// //     app.use(await Noco.init({}, httpServer, app));
+// //   } catch (e) {
+// //     console.log(e);
+// //   }
+// // })();
+
+// const httpServer = app.listen(process.env.PORT || 8080);
+
+// // app.use(await Noco.init({}, httpServer, app));
+
+// app.use(async function (req, res, next) {
+//   try {
+//     // const { Noco } = require('nocodb-daily');
+//     const { Noco } = require('nocodb');
+//     await Noco.init({}, httpServer, app);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// module.exports = app;
+
+let app = require('express')();
+
+const PORT = 8080;
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
+
+app.use(async function (req, res, next) {
+  try {
+    const { Noco } = require('nocodb');
+    await Noco.init({}, httpServer, app);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = app;
